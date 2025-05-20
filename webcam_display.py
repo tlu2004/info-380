3d7af4f17dc11f3a3aeb64cd7bb4a9dc1b01374f
Thomas Lu <149342460+tlu2004@users.noreply.github.com>
2025-05-20 14:33:50 -0700

    Add files via upload

---
+++ webcam_display.py
@@ -0,0 +1,25 @@
+import cv2
+
+# Open a connection to the default camera (0 is usually the default camera index)
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    print("Cannot open webcam")
+    exit()
+
+while True:
+    # Capture frame-by-frame from the camera
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to grab frame")
+        break
+
+    # Display the resulting frame in a window called "Webcam"
+    cv2.imshow('Webcam', frame)
+
+    # Wait for 1 ms and cpython webcam_display.pyheck if 'q' key is pressed to exit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything is done, release the capture and close windows
+cap.release()
+cv2.destroyAllWindows()
